c1d9b3244e2479a9e2a98732362fcc55f173f6cd
rafi andrea <rafiandrealesmana@gmail.com>
2024-12-01 11:41:03 +0700

    pages(home): create page home

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,59 +5,74 @@ export default {
     darkMode: ['class'],
     content: ['./src/**/*.tsx'],
     theme: {
-        extend: {
-            container: {
-                screens: {
-                    xl: '1200px',
-                },
-                center: true,
-            },
-            fontFamily: {
-                // sans: ["var(--font-geist-sans)", ...fontFamily.sans],
-                poppins: ['var(--font-poppins)', ...fontFamily.sans],
-            },
-            borderRadius: {
-                lg: 'var(--radius)',
-                md: 'calc(var(--radius) - 2px)',
-                sm: 'calc(var(--radius) - 4px)',
-            },
-            colors: {
-                sidebar: {
-                    DEFAULT: 'hsl(var(--sidebar-background))',
-                    foreground: 'hsl(var(--sidebar-foreground))',
-                    primary: 'hsl(var(--sidebar-primary))',
-                    'primary-foreground':
-                        'hsl(var(--sidebar-primary-foreground))',
-                    accent: 'hsl(var(--sidebar-accent))',
-                    'accent-foreground':
-                        'hsl(var(--sidebar-accent-foreground))',
-                    border: 'hsl(var(--sidebar-border))',
-                    ring: 'hsl(var(--sidebar-ring))',
-                },
-            },
-            keyframes: {
-                'accordion-down': {
-                    from: {
-                        height: '0',
-                    },
-                    to: {
-                        height: 'var(--radix-accordion-content-height)',
-                    },
-                },
-                'accordion-up': {
-                    from: {
-                        height: 'var(--radix-accordion-content-height)',
-                    },
-                    to: {
-                        height: '0',
-                    },
-                },
-            },
-            animation: {
-                'accordion-down': 'accordion-down 0.2s ease-out',
-                'accordion-up': 'accordion-up 0.2s ease-out',
-            },
-        },
+    	extend: {
+    		container: {
+    			screens: {
+    				xl: '1200px'
+    			},
+    			center: 'true'
+    		},
+    		fontFamily: {
+    			poppins: ['var(--font-poppins)', ...fontFamily.sans]
+    		},
+    		borderRadius: {
+    			lg: 'var(--radius)',
+    			md: 'calc(var(--radius) - 2px)',
+    			sm: 'calc(var(--radius) - 4px)'
+    		},
+    		colors: {
+    			sidebar: {
+    				DEFAULT: 'hsl(var(--sidebar-background))',
+    				foreground: 'hsl(var(--sidebar-foreground))',
+    				primary: 'hsl(var(--sidebar-primary))',
+    				'primary-foreground': 'hsl(var(--sidebar-primary-foreground))',
+    				accent: 'hsl(var(--sidebar-accent))',
+    				'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
+    				border: 'hsl(var(--sidebar-border))',
+    				ring: 'hsl(var(--sidebar-ring))'
+    			}
+    		},
+    		keyframes: {
+    			'accordion-down': {
+    				from: {
+    					height: '0'
+    				},
+    				to: {
+    					height: 'var(--radix-accordion-content-height)'
+    				}
+    			},
+    			'accordion-up': {
+    				from: {
+    					height: 'var(--radix-accordion-content-height)'
+    				},
+    				to: {
+    					height: '0'
+    				}
+    			},
+    			marquee: {
+    				from: {
+    					transform: 'translateX(0)'
+    				},
+    				to: {
+    					transform: 'translateX(calc(-100% - var(--gap)))'
+    				}
+    			},
+    			'marquee-vertical': {
+    				from: {
+    					transform: 'translateY(0)'
+    				},
+    				to: {
+    					transform: 'translateY(calc(-100% - var(--gap)))'
+    				}
+    			}
+    		},
+    		animation: {
+    			'accordion-down': 'accordion-down 0.2s ease-out',
+    			'accordion-up': 'accordion-up 0.2s ease-out',
+    			marquee: 'marquee var(--duration) infinite linear',
+    			'marquee-vertical': 'marquee-vertical var(--duration) linear infinite'
+    		}
+    	}
     },
     plugins: [require('tailwindcss-animate')],
 } satisfies Config;
