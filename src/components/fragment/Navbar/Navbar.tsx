9af8adb68775caac23139c7fc0bac40d0191894e
Aldy Ibnu Faizal <98567736+Aldyfaisal80@users.noreply.github.com>
2024-12-07 13:56:42 +0700

    feat(config): allow loading images from Cloudinary (#21)

--- src/components/fragment/Navbar/Navbar.tsx
+++ src/components/fragment/Navbar/Navbar.tsx
@@ -2,7 +2,7 @@ import { NavbarIcon } from './NavbarIcon';
 import { NavbarMenu } from './NavbarMenu';
 export const Navbar = () => {
     return (
-        <nav className="flex items-center justify-between px-5 py-2">
+        <nav className="flex items-center justify-between px-20 py-2">
             <NavbarIcon />
             <NavbarMenu />
         </nav>
