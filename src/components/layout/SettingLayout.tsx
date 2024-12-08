6ebe1679eb57fdf879637ad92cf06274d986f62a
rafi andrea <rafiandrealesmana@gmail.com>
2024-12-08 11:23:41 +0700

    feat(component): update layout for NavSetting components

--- src/components/layout/SettingLayout.tsx
+++ src/components/layout/SettingLayout.tsx
@@ -11,7 +11,7 @@ export const SettingLayout: React.FC<SettingLayoutProps> = props => {
         <>
             <Navbar />
             <Container className="pt-5 flex gap-2">
-                <nav>
+                <nav className='border-r border-zinc-200'>
                     <NavSetting />
                 </nav>
                 <main className="w-full px-5 pt-2">{children}</main>
