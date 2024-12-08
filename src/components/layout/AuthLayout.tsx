febb1a03d29aa1c67ecb3d2151a653f7705defc8
rafi andrea <rafiandrealesmana@gmail.com>
2024-12-08 10:12:31 +0700

    feat(auth): update AuthLayout UI and add AuthLayoutHero component

--- src/components/layout/AuthLayout.tsx
+++ src/components/layout/AuthLayout.tsx
@@ -1,5 +1,5 @@
-import Image from "next/image";
 import { NavbarIcon } from "@/components/fragment/navbar/NavbarIcon";
+import { AuthLayoutHero } from "@/components/fragment/AuthLayoutHero";
 
 type AuthLayoutProps = {
     children: React.ReactNode;
@@ -14,13 +14,7 @@ export const AuthLayout: React.FC<AuthLayoutProps> = props => {
                 <NavbarIcon />
             </div>
             <div className='flex h-screen justify-center items-center w-[52%]'>
-                <Image
-                    className="w-full h-full object-cover opacity-85"
-                    src={"https://res.cloudinary.com/dkmlayydv/image/upload/v1733625628/q5gsgr2v2krtuqihrdna.jpg"}
-                    alt="Auth Background"
-                    width={1500}
-                    height={1500}
-                />
+                <AuthLayoutHero />
             </div>
             <div className='flex justify-center items-center w-[48%]'>
                 <div
