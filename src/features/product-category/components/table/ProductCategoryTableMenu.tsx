a7d635a8071f77c13762086c9b1c192532f46a22
avnexcode <axnvee18@gmail.com>
2024-12-14 14:26:00 +0700

    feat!: I don't know what I'm doing

--- src/features/product-category/components/table/ProductCategoryTableMenu.tsx
+++ src/features/product-category/components/table/ProductCategoryTableMenu.tsx
@@ -5,8 +5,7 @@ import {
     DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
 import { DeleteProductCategoryButton } from '@/features/product-category/components/button/DeleteProductCategoryButton';
-import { Ellipsis, Pencil, Trash2, View } from 'lucide-react';
-<<<<<<< HEAD
+import { Ellipsis, View } from 'lucide-react';
 import { UpdateProductCategoryDialog } from '../dialog/UpdateProductCategoryDialog';
 
 export const ProductCategoryTableMenu = () => {
@@ -22,37 +21,11 @@ export const ProductCategoryTableMenu = () => {
                         <View />
                         Detail
                     </DropdownMenuItem>
-                    <DropdownMenuItem>{/* <Pencil /> */}</DropdownMenuItem>
-                    <DropdownMenuItem>
-                        <Trash2 />
-                        Delete
+                    <DropdownMenuItem asChild>
+                        <DeleteProductCategoryButton id="asd" />
                     </DropdownMenuItem>
                 </DropdownMenuContent>
             </DropdownMenu>
         </>
-=======
-import { as } from './../../../../../.next/static/chunks/51898_axios_7a7c63._';
-
-export const ProductCategoryTableMenu = () => {
-    return (
-        <DropdownMenu>
-            <DropdownMenuTrigger className="rounded-full">
-                <Ellipsis size={20} strokeWidth={2} />
-            </DropdownMenuTrigger>
-            <DropdownMenuContent className="[&>*]:cursor-pointer">
-                <DropdownMenuItem>
-                    <View />
-                    Detail
-                </DropdownMenuItem>
-                <DropdownMenuItem>
-                    <Pencil />
-                    Edit
-                </DropdownMenuItem>
-                <DropdownMenuItem asChild>
-                    <DeleteProductCategoryButton id="asd" />
-                </DropdownMenuItem>
-            </DropdownMenuContent>
-        </DropdownMenu>
->>>>>>> 59c4c8f25abfd07578e9eb4c5b99484046cddd10
     );
 };
