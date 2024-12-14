253845d6a58bb8f0420e0157061a599172574cb8
rafi andrea <rafiandrealesmana@gmail.com>
2024-12-14 13:33:57 +0700

    feat(store): create update store settings page

--- src/features/store/components/form/UpdateStoreForm.tsx
+++ src/features/store/components/form/UpdateStoreForm.tsx
@@ -3,26 +3,70 @@ import { UpdateStoreFormInner } from './UpdateStoreFormInner';
 import { updateStoreFormSchema, type UpdateStoreFormSchema } from '../../types';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { Form } from '@/components/ui/form';
+import { UpdateStoreFormLayout } from '../../layout/UpdateStoreFormLayout';
+import { useToast } from '@/hooks/use-toast';
+import { useUpdateStore } from '../../api/useUpdateStore';
+import { useAuth } from '@/features/auth/api/useAuth';
+import { UpdateStoreImageForm } from './UpdateStoreImageForm';
+import { useEffect } from 'react';
 
 export const UpdateStoreForm = () => {
+    const { toast } = useToast();
+    const { data: user } = useAuth();
+    
     const form = useForm<UpdateStoreFormSchema>({
         defaultValues: {
             name: '',
+            description: '',
+            bank_name: '',
+            bank_account: '',
+            bank_holder: '',
         },
         resolver: zodResolver(updateStoreFormSchema),
     });
 
-    const onsubmit = (values: UpdateStoreFormSchema) => console.log(values);
+    const onsubmit = (values: UpdateStoreFormSchema) => updateStore(values);
+
+    const { mutate: updateStore, isPending: isUpdateStoreSettingPending } =
+        useUpdateStore({
+            id: user?.store_id ?? undefined,
+            onSuccess: async () => {
+                toast({
+                    title: 'Success',
+                    description: 'Success update store setting',
+                });
+            },
+            onError: async error => {
+                toast({
+                    title: 'Success',
+                    variant: 'destructive',
+                    description: error.response?.data.error ?? error.message,
+                });
+            },
+        });
+
+    useEffect(() => {
+        if (user) {
+            form.reset({
+                name: user.store?.name,
+                description: user.store?.description,
+                bank_name: user.store?.bank_name,
+                bank_account: user.store?.bank_account,
+                bank_holder: user.store?.bank_holder,
+            });
+        }
+    }, [form, user]);
 
     return (
-        <>
+        <UpdateStoreFormLayout isPending={isUpdateStoreSettingPending}>
+            <UpdateStoreImageForm />
             <Form {...form}>
                 <UpdateStoreFormInner
-                    form_id="asd"
+                    form_id="update-store-setting"
                     form={form}
                     onSubmit={onsubmit}
                 />
             </Form>
-        </>
+        </UpdateStoreFormLayout>
     );
 };
--- src/features/store/components/form/UpdateStoreFormInner.tsx
+++ src/features/store/components/form/UpdateStoreFormInner.tsx
@@ -8,6 +8,7 @@ import {
 import { Input } from '@/components/ui/input';
 import { type UseFormReturn } from 'react-hook-form';
 import { type UpdateStoreFormSchema } from '../../types';
+import { Textarea } from '@/components/ui/textarea';
 
 type UpdateStoreFormInnerProps = {
     form_id: string;
@@ -18,7 +19,11 @@ type UpdateStoreFormInnerProps = {
 export const UpdateStoreFormInner = (props: UpdateStoreFormInnerProps) => {
     const { form_id, form, onSubmit } = props;
     return (
-        <form id={form_id} onSubmit={form.handleSubmit(onSubmit)}>
+        <form
+            id={form_id}
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="w-full flex flex-col gap-y-5"
+        >
             <div>
                 <FormField
                     control={form.control}
@@ -33,6 +38,77 @@ export const UpdateStoreFormInner = (props: UpdateStoreFormInnerProps) => {
                         </FormItem>
                     )}
                 />
+            </div>
+            <div>
+                <FormField
+                    control={form.control}
+                    name="description"
+                    render={({ field }) => (
+                        <FormItem className="flex flex-col gap-2">
+                            <FormLabel>Description</FormLabel>
+                            <FormControl>
+                                <Textarea
+                                    placeholder="description"
+                                    {...field}
+                                />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+            </div>
+            <div className="flex flex-col lg:flex-row gap-4">
+                <div className="lg:w-1/3 w-full">
+                    <FormField
+                        control={form.control}
+                        name="bank_name"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Bank Name</FormLabel>
+                                <FormControl>
+                                    <Input placeholder="bank_name" {...field} />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                </div>
+                <div className="lg:w-1/3 w-full">
+                    <FormField
+                        control={form.control}
+                        name="bank_account"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Bank Account</FormLabel>
+                                <FormControl>
+                                    <Input
+                                        placeholder="bank_account"
+                                        {...field}
+                                    />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                </div>
+                <div className="lg:w-1/3 w-full">
+                    <FormField
+                        control={form.control}
+                        name="bank_holder"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Bank Holder</FormLabel>
+                                <FormControl>
+                                    <Input
+                                        placeholder="Bank Holder"
+                                        {...field}
+                                    />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                </div>
             </div>
         </form>
     );
