59c4c8f25abfd07578e9eb4c5b99484046cddd10
Rafi Andrea <134251284+Apchier@users.noreply.github.com>
2024-12-14 13:45:08 +0700

    feat(store): create update store settings page (#31)

---
+++ src/features/store/components/form/UpdateStoreImageForm.tsx
@@ -0,0 +1,16 @@
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { Button } from '@/components/ui/button';
+
+export const UpdateStoreImageForm = () => {
+    return (
+        <div className="w-full flex flex-col items-start gap-5">
+            <Avatar className="w-36 h-36">
+                <AvatarImage src="https://placehold.co/400x400" alt={''} />
+                <AvatarFallback>{''}</AvatarFallback>
+            </Avatar>
+            <Button variant="outline" size="sm" className="w-full max-w-lg">
+                Upload Foto
+            </Button>
+        </div>
+    );
+};
