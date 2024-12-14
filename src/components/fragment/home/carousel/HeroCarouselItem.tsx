3f15d2e750ecab968f2ba8d46387f3b90977bf2f
aldyfaisal80 <aldyfaisal80@gmail.com>
2024-12-14 15:15:56 +0700

    feat(home): add banner image to homepage

--- src/components/fragment/home/carousel/HeroCarouselItem.tsx
+++ src/components/fragment/home/carousel/HeroCarouselItem.tsx
@@ -1,5 +1,7 @@
 import { Card, CardContent } from '@/components/ui/card';
 import { CarouselItem } from '@/components/ui/carousel';
+import { Fullscreen } from 'lucide-react';
+import Image from 'next/image';
 
 type HeroCarouselItemProps = {
     label: string;
@@ -9,11 +11,18 @@ export const HeroCarouselItem = (props: HeroCarouselItemProps) => {
     return (
         <CarouselItem>
             <div className="p-1">
-                <Card>
-                    <CardContent className="flex h-[400px] w-full items-center justify-center p-6">
-                        <span className="text-4xl font-semibold">
-                            {props.label}
-                        </span>
+                <Card className="border-none outline-none shadow-none">
+                    <CardContent className="flex h-[470px] w-full items-center justify-center p-6">
+                        <div className="overflow-hidden h-[470px] rounded-lg w-full">
+                            <Image
+                                src="https://res.cloudinary.com/dkmlayydv/image/upload/v1734160926/BannerFoodtzy_1_ecebvk.png"
+                                alt={props.label}
+                                width={1300}
+                                height={400}
+                                className="object-cover w-full h-full"
+                                unoptimized
+                            />
+                        </div>
                     </CardContent>
                 </Card>
             </div>
