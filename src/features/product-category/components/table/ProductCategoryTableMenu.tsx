2ff408a6353b5f5c123c2d37c590c28fd6509894
shodeq <alfiananwarshodiqi@gmail.com>
2024-12-14 13:39:25 +0700

    feat:(Product-Category) add product category update and dashboard edit functionality

--- src/features/product-category/components/table/ProductCategoryTableMenu.tsx
+++ src/features/product-category/components/table/ProductCategoryTableMenu.tsx
@@ -5,27 +5,28 @@ import {
     DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
 import { Ellipsis, Pencil, Trash2, View } from 'lucide-react';
+import { UpdateProductCategoryDialog } from '../dialog/UpdateProductCategoryDialog';
 
 export const ProductCategoryTableMenu = () => {
     return (
-        <DropdownMenu>
-            <DropdownMenuTrigger className="rounded-full">
-                <Ellipsis size={20} strokeWidth={2} />
-            </DropdownMenuTrigger>
-            <DropdownMenuContent className="[&>*]:cursor-pointer">
-                <DropdownMenuItem>
-                    <View />
-                    Detail
-                </DropdownMenuItem>
-                <DropdownMenuItem>
-                    <Pencil />
-                    Edit
-                </DropdownMenuItem>
-                <DropdownMenuItem>
-                    <Trash2 />
-                    Delete
-                </DropdownMenuItem>
-            </DropdownMenuContent>
-        </DropdownMenu>
+        <>
+            <UpdateProductCategoryDialog />
+            <DropdownMenu>
+                <DropdownMenuTrigger className="rounded-full">
+                    <Ellipsis size={20} strokeWidth={2} />
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className="[&>*]:cursor-pointer">
+                    <DropdownMenuItem>
+                        <View />
+                        Detail
+                    </DropdownMenuItem>
+                    <DropdownMenuItem>{/* <Pencil /> */}</DropdownMenuItem>
+                    <DropdownMenuItem>
+                        <Trash2 />
+                        Delete
+                    </DropdownMenuItem>
+                </DropdownMenuContent>
+            </DropdownMenu>
+        </>
     );
 };
