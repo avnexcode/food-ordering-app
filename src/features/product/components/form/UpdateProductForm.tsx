e51a6fe69ced2c0d6aa992fffd372f757ea4b740
jihannkn <khanzajihan@gmail.com>
2024-12-14 14:18:23 +0700

    feat(product): add new components for product editing

--- src/features/product/components/form/UpdateProductForm.tsx
+++ src/features/product/components/form/UpdateProductForm.tsx
@@ -1,3 +1,64 @@
-export const UpdateProductForm = () => {
-    return;
+import { Form } from '@/components/ui/form';
+import { useForm } from 'react-hook-form';
+import { UpdateProductFormInner } from './UpdateProductFormInner';
+import {
+    updateProductFormSchema,
+    type UpdateProductFormSchema,
+} from '../../types';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useUpdateProduct } from '../../api/useUpdateProduct';
+import { useToast } from '@/hooks/use-toast';
+
+type UpdateProductFormProps = {
+    setIsPending: (isPending: boolean) => void;
+};
+
+export const UpdateProductForm = (props: UpdateProductFormProps) => {
+    const { setIsPending } = props;
+    const { toast } = useToast();
+    const form = useForm<UpdateProductFormSchema>({
+        defaultValues: {
+            name: '',
+            price: 0,
+            description: '',
+            images: [],
+            stock: 0,
+            weight: 0,
+            category_id: '',
+        },
+        resolver: zodResolver(updateProductFormSchema),
+    });
+
+    const onSubmit = (values: UpdateProductFormSchema) => {
+        setIsPending(isUpdateProductPending);
+        console.log(values);
+    };
+
+    const { mutate: updateProduct, isPending: isUpdateProductPending } =
+        useUpdateProduct({
+            onSuccess: async () => {
+                setIsPending(isUpdateProductPending);
+                toast({
+                    title: 'Success',
+                    description: 'Success update product',
+                });
+            },
+            onError: async error => {
+                toast({
+                    title: 'Error',
+                    description: error.response?.data?.error ?? error.message,
+                    variant: 'destructive',
+                });
+            },
+        });
+
+    return (
+        <Form {...form}>
+            <UpdateProductFormInner
+                form_id="update-product-form"
+                form={form}
+                onSubmit={onSubmit}
+            />
+        </Form>
+    );
 };
--- src/features/product/components/form/UpdateProductFormInner.tsx
+++ src/features/product/components/form/UpdateProductFormInner.tsx
@@ -1,3 +1,146 @@
-export const UpdateProductFormInner = () => {
-    return;
+import {
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { type UseFormReturn } from 'react-hook-form';
+import type { UpdateProductFormSchema } from '../../types';
+import { UpdateProductCategoryOption } from '../option/UpdateProductCategoryOption';
+
+type UpdateProductFormInnerProps = {
+    form_id: string;
+    form: UseFormReturn<UpdateProductFormSchema>;
+    onSubmit: (values: UpdateProductFormSchema) => void;
+};
+
+export const UpdateProductFormInner = (props: UpdateProductFormInnerProps) => {
+    const { form_id, form, onSubmit } = props;
+
+    return (
+        <form
+            id={form_id}
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="flex flex-col gap-2 w-full"
+        >
+            <div>
+                <FormField
+                    control={form.control}
+                    name="name"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Name</FormLabel>
+                            <FormControl>
+                                <Input
+                                    placeholder="Input product name"
+                                    {...field}
+                                />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+            </div>
+            <div>
+                <FormField
+                    control={form.control}
+                    name="price"
+                    render={({ field: { onChange, value, ...field } }) => (
+                        <FormItem>
+                            <FormLabel>Price</FormLabel>
+                            <FormControl>
+                                <div className="relative flex items-center">
+                                    <span className="absolute left-3 text-gray-500">
+                                        Rp.
+                                    </span>
+                                    <Input
+                                        type="text"
+                                        placeholder="0"
+                                        className="pl-12"
+                                        {...field}
+                                        onChange={e => {
+                                            const rawValue =
+                                                e.target.value.replace(
+                                                    /[^\d]/g,
+                                                    '',
+                                                );
+                                            const numberValue =
+                                                Number(rawValue);
+                                            onChange(numberValue);
+                                        }}
+                                    />
+                                </div>
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+            </div>
+            <div className="flex gap-2 items-center">
+                <UpdateProductCategoryOption form={form} />
+                <FormField
+                    control={form.control}
+                    name="images"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormControl>
+                                <Input type="file" placeholder="" {...field} />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+            </div>
+            <div>
+                <FormField
+                    control={form.control}
+                    name="description"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Description</FormLabel>
+                            <FormControl>
+                                <Input
+                                    placeholder="Input product description"
+                                    {...field}
+                                />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+            </div>
+            <div>
+                <FormField
+                    control={form.control}
+                    name="stock"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Stock</FormLabel>
+                            <FormControl>
+                                <Input placeholder="0" {...field} />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+            </div>
+            <div>
+                <FormField
+                    control={form.control}
+                    name="weight"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Weight</FormLabel>
+                            <FormControl>
+                                <Input placeholder="0" {...field} />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+            </div>
+        </form>
+    );
 };
